675d87f54e3a7434ca0693016d3348c41f035fba
Eko Bayu <eko.bayu@ogilvy.com>
2025-04-23 14:58:25 +0800

    initial node js recapctha v3

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/submit-form', async (req, res) => {
+  const token = req.body['g-recaptcha-response']
+  const secret = '6LfPKhorAAAAABgwvgW5s1AWOVZz7PWDitHiH0CK'
+
+  const response = await axios.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`
+  )
+
+  if (response.data.success && response.data.score >= 0.5) {
+    // Form submission is valid
+    res.send('Success!')
+  } else {
+    // Likely a bot
+    res.status(400).send('Failed reCAPTCHA')
+  }
+})
+
+app.listen(3000, () => console.log('Server running'))
